8f7a84f84b98d84c780188f9ecd9de822012e4ba
hungnt <nguyenthanhhung28092002@gmail.com>
2024-10-16 23:18:08 +0700

    [court]search and filter api

---
+++ Application/Handler/Courts/Search.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Core;
+using Domain.Entity;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Handler.Courts
+{
+    public class Search
+    {
+        public class Query : IRequest<Result<List<Court>>>
+        {
+            public string value { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<Court>>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Result<List<Court>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var courts = await _context.Courts.Where(x => x.CourtName.Contains(request.value)).ToListAsync(cancellationToken);
+                return Result<List<Court>>.Success(courts);
+            }
+        }
+    }
+}
