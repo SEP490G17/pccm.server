c17c80761b40b2bc2e951f940a829e04fe36a055
ng.lonqvu <longvu131102@gmail.com>
2024-10-28 17:11:56 +0700

    update edit service

--- Application/Handler/Services/Edit.cs
+++ Application/Handler/Services/Edit.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Domain.Entity;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Handler.Services
@@ -35,12 +36,15 @@ namespace Application.Handler.Services
             public async Task<Result<ServiceDto>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var service = _mapper.Map<Service>(request.Service);
-                var serviceExist = await _context.Services.FindAsync(request.Service.Id);
-                _mapper.Map(service, serviceExist);
+                var serviceExist = await _context.Services.Include(s => s.CourtCluster).FirstOrDefaultAsync(s => s.Id == request.Service.Id);
+                if (serviceExist == null)
+                    return Result<ServiceDto>.Failure("Fail to edit service");
+                service.CourtCluster = serviceExist.CourtCluster;
+                _mapper.Map(request.Service, serviceExist);
                 var result = await _context.SaveChangesAsync() > 0;
                 if (!result) return Result<ServiceDto>.Failure("Fail to edit service");
-                var entity = _context.Entry(service).Reference(s=>s.CourtCluster);
-                var response = _mapper.Map<ServiceDto>(entity);
+                var updateService = _context.Entry(service).Entity;
+                var response = _mapper.Map<ServiceDto>(serviceExist);
                 return Result<ServiceDto>.Success(response);
             }
         }
