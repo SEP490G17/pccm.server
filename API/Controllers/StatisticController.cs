78c3ba6061c37c0921b5f26ea99e16d55a7c7083
Mrhieu123 <128633008+Mrhieu123@users.noreply.github.com>
2024-10-28 21:24:41 +0700

    total-income

---
+++ API/Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Handler.Statistics;
+using Application.SpecParams;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class StatisticController : BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpGet("income")]
+        public async Task<IActionResult> GetIncome(string? year, string? month, string? courtclusterId, CancellationToken ct)
+        {
+            return HandleResult(await Mediator.Send(new Income.Query() { Year = year, Month = month, CourtClusterId = courtclusterId }, ct));
+        }
+    }
+}
