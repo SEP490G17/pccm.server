3eda747a2029f261f60dbc3a2b6620a7fc8d0750
trongnp <trongnp144@gmail.com>
2024-11-22 09:11:30 +0700

    [hot-fix] vnpay

--- Infrastructure/Payment/VnPayService.cs
+++ Infrastructure/Payment/VnPayService.cs
@@ -23,24 +23,28 @@ namespace Infrastructure.Payment
         {
             var tick = DateTime.Now.ToString();
             var ipAddress = Utils.GetIpAddress(_httpContextAccessor.HttpContext);
-            var createDate = DateTime.Now;
+            var vietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Bangkok Standard Time");
+
+            var utcNow = DateTime.UtcNow;
+            var createDate = TimeZoneInfo.ConvertTimeFromUtc(utcNow, vietnamTimeZone);
+
             var vnPay = new VnPayLibrary();
             vnPay.AddRequestData("vnp_Version", "2.1.1");
             vnPay.AddRequestData("vnp_Command", "pay");
             vnPay.AddRequestData("vnp_TmnCode", _settings.TmnCode);
             vnPay.AddRequestData("vnp_Amount", ((int)(amount * 100)).ToString());
-            vnPay.AddRequestData("vnp_CreateDate",createDate.ToString("yyyyMMddHHmmss"));
-            vnPay.AddRequestData("vnp_ExpireDate",createDate.AddMinutes(30).ToString("yyyyMMddHHmmss"));
+            vnPay.AddRequestData("vnp_CreateDate", createDate.ToString("yyyyMMddHHmmss"));
+            vnPay.AddRequestData("vnp_ExpireDate", createDate.AddMinutes(30).ToString("yyyyMMddHHmmss"));
             vnPay.AddRequestData("vnp_CurrCode", "VND");
             vnPay.AddRequestData("vnp_TxnRef", $"{billPaymentId}_{(int)type}_{tick}");
-         
+
             vnPay.AddRequestData("vnp_Locale", "vn");
             vnPay.AddRequestData("vnp_OrderInfo", $"Thanh toan don hang: {billPaymentId}");
             vnPay.AddRequestData("vnp_OrderType", $"{type.ToString()}");
             vnPay.AddRequestData("vnp_ReturnUrl", _settings.ReturnUrl);
-            vnPay.AddRequestData("vnp_IpAddr", string.IsNullOrEmpty(ipAddress)? "127.0.0.1": ipAddress);
+            vnPay.AddRequestData("vnp_IpAddr", string.IsNullOrEmpty(ipAddress) ? "127.0.0.1" : ipAddress);
 
-            return vnPay.CreateRequestUrl(_settings.Url,_settings.HashSecret);
+            return vnPay.CreateRequestUrl(_settings.Url, _settings.HashSecret);
         }
 
         public bool VerifyVnPaySignature(VnPayCallbackDto callback)
